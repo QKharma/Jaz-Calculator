dd9b604bed3153d04640e89764df2051f1644956
Yumi <yuma.gugerli@gmail.com>
2022-07-13 16:35:11 +0200

    :bug: Fix pressing delete on time inputs

--- src/components/TimeInput.tsx
+++ src/components/TimeInput.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import msToString from '../lib/msToString'
 
 interface TimeInputProps {
@@ -8,6 +8,11 @@ interface TimeInputProps {
 }
 
 const TimeInput = (props: TimeInputProps) => {
+  const resetInput = (e: React.KeyboardEvent) => {
+    e.preventDefault()
+    props.setTime(0)
+  }
+
   return (
     <div className='flex flex-row justify-end lg:mr-20'>
       <p className='text-white mr-10 whitespace-nowrap'>{props.name}</p>
@@ -17,7 +22,9 @@ const TimeInput = (props: TimeInputProps) => {
         onChange={(e) => {
           props.setTime(e.target.valueAsNumber)
         }}
+        onKeyDown={(e) => e.key === 'Delete' && resetInput(e)}
         value={msToString(props.time)}
+        required
       ></input>
     </div>
   )
